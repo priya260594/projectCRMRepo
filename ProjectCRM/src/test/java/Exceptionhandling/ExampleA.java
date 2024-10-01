0cd04c6750cfbd86637683794ec34311c0a5de25
devis <devis@Devi>
2024-10-01 12:05:51 +0530

    commited exampleA

---
+++ ProjectCRM/src/test/java/Exceptionhandling/ExampleA.java
@@ -0,0 +1,11 @@
+package Exceptionhandling;
+
+public class ExampleA {
+
+	public static void main(String[] args) {
+		System.out.println("example 1 is created");
+		// TODO Auto-generated method stub
+
+	}
+
+}
